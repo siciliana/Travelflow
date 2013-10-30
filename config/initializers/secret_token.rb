fcdbf41c99fdb629f50addbbdaea2be69430d940
cdcueva <msh345@gmail.com>
2013-10-30 14:44:52 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Travelflow::Application.config.secret_token = '233e2b17b187187ea25963a6c27da03c625ce1cab31aca91e0e45372485ef093292c0d59ba9b3094d3baa05396c3a728de4ed3e5ac6d0de892ba083858394c27'
