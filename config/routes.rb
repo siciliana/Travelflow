43b2b3f9e739a365b33c4faaaae49b65aaa3ae03
cdcueva <msh345@gmail.com>
2013-10-30 15:39:00 -0500

    root :to updated

--- config/routes.rb
+++ config/routes.rb
@@ -62,6 +62,7 @@ Travelflow::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
 
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
