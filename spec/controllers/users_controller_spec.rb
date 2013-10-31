5bda8d38131b914e271d8b37aedf617bc7e3f447
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-31 13:53:33 -0500

    created tests for user_controller, sign up feature

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe UsersController do
+  it "should render the new user page successfully" do
+    get :new
+    response.status.should eq 200
+  end
+
+  it "allows a user to signup" do
+    User.count.should == 0
+    create_user
+    User.count.should == 1
+  end
+
+  it "signing up requires an email address" do
+    User.count.should == 0
+    create_user(:email => nil)
+    User.count.should == 0
+  end
+
+  it "signing up requires a password" do
+    User.count.should == 0
+    create_user(:password => nil)
+    User.count.should == 0
+  end
+
+  it "if user is saved, redirect" do
+    create_user 
+    response.should be_redirect
+  end
+
+  it "if user is not saved, rerender form" do
+    create_user(:password => nil)
+    response.should render_template("new")
+  end
+  
+  def create_user(options = {})
+    post :create, :user => {:email => "email@email.com", :password =>"password"}.merge(options)
+  end
+end
+
+
