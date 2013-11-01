4f6c15c3a22a47bae79d4d000ca55eb901cb81fa
cdcueva <msh345@gmail.com>
2013-10-31 19:18:06 -0500

    Added ajax functionality to answer form on question show page

---
+++ app/assets/javascripts/question.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  console.log("are we even on the freaking page?")
+
+ $('form').hide(); 
+  $('#button_id').click(function() {
+   $('form').show();
+
+  });
+ 
+});
