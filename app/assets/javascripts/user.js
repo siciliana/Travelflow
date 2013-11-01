f7b3225aa4b1257da2f1911394b7944916422ea8
cdcueva <msh345@gmail.com>
2013-10-31 20:29:10 -0500

    ajax login is now functional

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,13 +1,3 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
-$(document).ready(function(){
-  console.log("are we even on the freaking page?")
-
- $('#new_user_form').hide(); 
-  $('.ajax_form_login').click(function() {
-   $('#new_user_form').show();
-
-  });
- 
-});
