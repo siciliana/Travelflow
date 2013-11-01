07a8e32fd54b0f57991be62bb37b13e8ab6472d9
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-31 21:45:44 -0500

    added mini-photos to user question and answer posts

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -8,6 +8,7 @@ class AnswersController < ApplicationController
     @question = Question.find(params[:question_id])
     @answer = current_user.answers.new(params[:answer])
     @answer.question = @question
+    
 
     if @answer.save
       redirect_to question_path(@question)
